eaadefc9891a211469ccade71002710043a9150e
laurentst100 <atebalaurent0@gmail.com>
2025-09-07 16:44:11 -0700

    Update TelemetryData.java

--- src/main/java/com/example/demo/model/TelemetryData.java
+++ src/main/java/com/example/demo/model/TelemetryData.java
@@ -1,8 +1,8 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 
 @Entity
