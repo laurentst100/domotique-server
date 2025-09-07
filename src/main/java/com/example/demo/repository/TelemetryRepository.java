771ec8530cd8a605bbdb3a1b5d951e064254ae91
laurentst100 <atebalaurent0@gmail.com>
2025-09-07 16:20:16 -0700

    Update TelemetryRepository.java

--- src/main/java/com/example/demo/repository/TelemetryRepository.java
+++ src/main/java/com/example/demo/repository/TelemetryRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.moddel.TelemetryData;
+import com.example.demo.model.TelemetryData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
