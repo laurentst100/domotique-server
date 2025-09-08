73e2e5e5ec1d69d9274666e8bd62ee4e47004c72
laurentst100 <atebalaurent0@gmail.com>
2025-09-07 20:51:30 -0700

    Update TelemetryService.java

--- src/main/java/com/example/demo/service/TelemetryService.java
+++ src/main/java/com/example/demo/service/TelemetryService.java
@@ -25,4 +25,11 @@ public class TelemetryService {
     public List<TelemetryData> getLatestTelemetryForAllDevices() {
         return telemetryRepository.findLatestForEachDevice();
     }
+    public List<HourlyPowerSummary> getHourlySummaryForLast24Hours() {
+    // On définit la période (les dernières 24 heures)
+    Instant since = Instant.now().minus(24, ChronoUnit.HOURS);
+    
+    // On appelle la nouvelle méthode du repository que tu as créée
+    return telemetryRepository.findHourlyPowerSummary(since);
+  }
 }
